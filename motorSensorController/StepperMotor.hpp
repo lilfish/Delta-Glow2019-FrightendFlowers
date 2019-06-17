f3e3714da1918e16c32486134c062e4cc8a66506
Geert Boer <g.boer@student.fontys.nl>
2019-06-17 20:07:16 +0200

    motor working. TODO: HEIGHT VALUES

--- motorSensorController/StepperMotor.hpp
+++ motorSensorController/StepperMotor.hpp
@@ -3,35 +3,98 @@
 #include "Enums.hpp"
 #include <Stepper.h>
 
+#define STEPPER_MAX 950
+
 class StepperMotor : public IMotor
 {
 private:
     Stepper* stepper;
     States state;
+    int currentPos;
+
+    int m1a;
+    int m1b; 
+    int m2a; 
+    int m2b;
 public:
     StepperMotor(int stepsPerRev, int m1a, int m1b, int m2a, int m2b);
     void Update();
     void SetState(States state);
+    void GoToPosition(int pos);
+    void FallDown();
     ~StepperMotor();
 };
 
 StepperMotor::StepperMotor(int stepsPerRev, int m1a, int m1b, int m2a, int m2b) : IMotor()
 {
     stepper = new Stepper(stepsPerRev, m1a, m1b, m2a, m2b);
+    stepper->setSpeed(60);
     state = CALM;
+    this->m1a = m1a;
+    this->m1b = m1b; 
+    this->m2a = m2a; 
+    this->m2b = m2b;
+    currentPos = 0;
 }
 
 StepperMotor::~StepperMotor()
 {
-    //REMOVE STEPPER*;
+    delete stepper;
+}
+
+void StepperMotor::GoToPosition(int pos)
+{
+    if (pos <= 0 || pos > STEPPER_MAX)
+    {
+        return;
+    }
+    stepper->step(pos - currentPos);
+    currentPos = pos;
+}
+
+void StepperMotor::FallDown()
+{
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW);
+    currentPos = 0;
 }
 
 void StepperMotor::Update()
 {
-    //MOVETOPOS;
+    int posToMoveTo = 0;
+    switch (state)
+    {
+        case CALM:
+            posToMoveTo = 700;
+            break;
+        case RESTLESS1:
+            posToMoveTo = 750;
+            break;
+        case RESTLESS2:
+            posToMoveTo = 800;
+            break;
+        case RESTLESS3:
+            posToMoveTo = 850;
+            break;
+        case OUT:
+            FallDown();
+            break;
+        default:
+            break;
+    }
+    Serial.println("postomoc");
+    Serial.println(posToMoveTo);
+
+    Serial.println("stepstotake");
+    int stepsToTake = posToMoveTo - currentPos;
+    Serial.println(stepsToTake);
+    stepper->step(stepsToTake);
+    currentPos = currentPos + stepsToTake;
 }
 
 void StepperMotor::SetState(States state)
 {
-    ;
+    this->state = state;
 }
--- motorSensorController/motorSensorController.ino
+++ motorSensorController/motorSensorController.ino
@@ -17,11 +17,26 @@ void setup() {
   communicator = new SerialCommunicator(9600);
   sensor = new UltrasonicDistanceSensor(12,13);
   motor = new StepperMotor(200,8,9,10,11);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  States x = sensor->GetState();
-  motor->SetState(x);
-  communicator->SetState(x);
+  // States x = sensor->GetState();
+  // motor->SetState(x);
+  // communicator->SetState(x);
+  Serial.println("ye");
+  delay(2000);
+  motor->SetState(CALM);
+  motor->Update();
+  delay(1000);
+  motor->SetState(RESTLESS2);
+  motor->Update();
+  delay(1000);
+  motor->SetState(RESTLESS1);
+  motor->Update();
+  delay(3000);
+  motor->SetState(OUT);
+  motor->Update();
+  delay(4000);
 }
