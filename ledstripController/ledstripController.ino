efdc400ad431a95e66daf2beb392e010782739ba
Fish <vincentvenhuizen1997@gmail.com>
2019-06-17 14:56:49 +0200

    Started the structure
    Made a begin on the structure, passing it to Geert.

---
+++ ledstripController/ledstripController.ino
@@ -0,0 +1,22 @@
+enum States {calm, restless1, restless2, restless3, out };
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void communicationCode() {
+  
+
+}
+
+void ledstripCode(States state) {
+  //state 0 = calm
+  //state 1 to 3 = restless (1 - 3)
+  //state 4 = out
+}
---
+++ motorSensorController/motorSensorController.ino
@@ -0,0 +1,24 @@
+enum States {calm, restless1, restless2, restless3, out };
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void sensorCode() {
+  //
+}
+
+void communicationCode() {
+
+}
+
+void motorCode(States state) {
+  //state 0 = calm
+  //state 1 to 3 = restless (1 - 3)
+  //state 4 = out
+}
