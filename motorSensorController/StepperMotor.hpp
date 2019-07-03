6140e9fcfdbb1608dce7c8a8b8ee3860acb7ee93
Geert Boer <g.boer@student.fontys.nl>
2019-07-03 21:38:18 +0200

    Stepper code finished :}

--- motorSensorController/StepperMotor.hpp
+++ motorSensorController/StepperMotor.hpp
@@ -5,10 +5,15 @@
 
 class StepperMotor
 {
-private:
+  private:
     Stepper* stepper;
     int currentHeight;
-public:
+
+    int m1a;
+    int m1b;
+    int m2a;
+    int m2b;
+  public:
     StepperMotor(int stepsPerRev, int m1a, int m1b, int m2a, int m2b);
     void Update();
     void SetState(States state);
@@ -21,47 +26,55 @@ public:
     void FallDown();
 };
 
-StepperMotor::StepperMotor(int stepsPerRev, int m1a, int m1b, int m2a, int m2b) : IMotor()
+StepperMotor::StepperMotor(int stepsPerRev, int m1a, int m1b, int m2a, int m2b)
 {
-    stepper = new Stepper(stepsPerRev, m1a, m1b, m2a, m2b);
-    stepper.SetSpeed(60);
+  stepper = new Stepper(stepsPerRev, m1a, m1b, m2a, m2b);
+  stepper->setSpeed(170);
 
-    pinMode(m1a, OUTPUT);
-    pinMode(m1b, OUTPUT);
-    pinMode(m2a, OUTPUT);
-    pinMode(m2b, OUTPUT);
+  pinMode(m1a, OUTPUT);
+  pinMode(m1b, OUTPUT);
+  pinMode(m2a, OUTPUT);
+  pinMode(m2b, OUTPUT);
 
-    state = CALM;
+  this->m1a = m1a;
+  this->m1b = m1b;
+  this->m2a = m2a;
+  this->m2b = m2b;
+
+  currentHeight = 0;
 }
 
 StepperMotor::~StepperMotor()
 {
-    delete stepper;
+  delete stepper;
 }
 
 bool StepperMotor::GoToHeight(int height)
 {
-    int posToMove = currentHeight + height;
-    stepper.step(posToMove);
-    return true;
+  int posToMove = height - currentHeight;
+  stepper->step(posToMove);
+  currentHeight = height;
+  return true;
 }
 
 bool StepperMotor::Shake(int intensity)
 {
-    stepper.step(intensity);
-    stepper.step(-1 * intensity);
-    return true;
+  stepper->step(intensity);
+  stepper->step(-1 * intensity);
+  return true;
 }
 
 int StepperMotor::CurrentHeight()
 {
-    return currentHeight;
+  return currentHeight;
 }
 
 void StepperMotor::FallDown()
 {
-    digitalWrite(m1a,LOW);
-    digitalWrite(m1b,LOW);
-    digitalWrite(m2a,LOW);
-    digitalWrite(m2b,LOW);
+  digitalWrite(m1a, LOW);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2a, LOW);
+  digitalWrite(m2b, LOW);
+  currentHeight = 0;
+  delay(8000);
 }
--- motorSensorController/motorSensorController.ino
+++ motorSensorController/motorSensorController.ino
@@ -1,20 +1,27 @@
-// #define DEBUG
-
 #include "Enums.hpp"
 
 #include "IMotor.hpp"
 #include "StepperMotor.hpp"
 
-StepperMotor motor(200,1,2,3,4);
+StepperMotor motor(200, 8, 9, 10, 11);
 
+int maxHeight = 2700;
 void setup()
 {
-// put your setup code here, to run once:
-
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  motor.GoToHeight(maxHeight - 300);
 }
+
 
 void loop()
 {
-    motor.GoToHeight(900);
-    motor.Shake(3);
+  motor.GoToHeight(maxHeight);
+  delay(2000);
+  motor.FallDown();
+  motor.GoToHeight(maxHeight - 300);
+  for (int i = 0; i < 40; i++)
+  {
+    motor.Shake(50);
+  }
 }
